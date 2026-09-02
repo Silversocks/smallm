50b02b0c40e06520c3e754848e043d8dbc45f650
Muhammed Shamil <muhammedshamil.kvn@gmail.com>
2026-09-02 23:19:51 +0530

    started on GPT2

--- gptmodel/batchtokenisationandharness.py
+++ gptmodel/batchtokenisationandharness.py
@@ -5,9 +5,10 @@ import tiktoken
 encoder = tiktoken.get_encoding("gpt2")
 
 batch = []
-txt1 = "Hello world, I am Muhammed Shamil"
+txt1 = "Hello world I am Person"
 txt2 = "This is a test sentence"
-
+print(txt1)
+print(txt2)
 batch.append(torch.tensor(encoder.encode(txt1)))
 batch.append(torch.tensor(encoder.encode(txt2)))
 batch = torch.stack(batch,dim=0)
