96fd0c88a94c7b915f64312f1f946634402e4905
Muhammed Shamil <muhammedshamil.kvn@gmail.com>
2026-09-03 23:46:32 +0530

    small changes

--- gptmodel/simplemodelgelupart.py
+++ gptmodel/simplemodelgelupart.py
@@ -5,9 +5,9 @@ class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.layers = nn.Sequential(
-        nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+        nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]), # a larger dimension is used in hiddenlayer because it expands the representation space, allowing for potentially more meaningful exploration for the model
         GELU(),
-        nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]), # The reason the layer has same input and output dimensions is because it becomes easier to combine with other layers without changing intermittent dimensions.
         )
     
     def forward(self, x):
--- nvim.fish
+++ nvim.fish
@@ -1,2 +1,3 @@
+# /usr/bin/fish
 source .venv/bin/activate.fish
 nvim .
